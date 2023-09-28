2f54c7666ae45ecda1042e538ab5e989fef17e7e
Eder-martinez <1722110109@utectulancingo.edu.mx>
2023-09-28 16:46:50 +0000

    Importar csv

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+
+
 
 app = FastAPI()
 
 
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"message": "Hello World"}
 
 @app.get("/personas")
 def get_personas():
     return {"id":1,"nombre": "Dejah"}
+
+@app.get("/v1/contactos")
+async def get_contactos(file: bytes = File("contactos.csv")):
+    # TODO read contactos.csv
+    # TODO JSON encode contactos.csv
+    # TODO save in response
+    response = []
+    return response
