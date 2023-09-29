82ab8a692cdba5fd2193f03ff821813d29b4529b
Eder-martinez <1722110109@utectulancingo.edu.mx>
2023-09-29 17:59:39 +0000

    Documentacion

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, status
 
 
 
@@ -8,16 +8,29 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
+    def root():
+    """
+    #Endpoint raíz
+    ##1-Status codes
+    *298-
+    *301-
+    """
     return {"message": "Hello World"}
 
 @app.get("/personas")
 def get_personas():
     return {"id":1,"nombre": "Dejah"}
 
-@app.get("/v1/contactos")
-async def get_contactos(file: bytes = File("contactos.csv")):
+@app.get(
+     "/v1/contactos",
+     status_code=201,
+     description="Endpoint raiz de la API Contactos",
+     summary="Endpoint raiz"
+     )
+
+async def get_contactos():
     # TODO read contactos.csv
     # TODO JSON encode contactos.csv
     # TODO save in response
-    response = [file("contactos.csv")]
+    response = []
     return response
