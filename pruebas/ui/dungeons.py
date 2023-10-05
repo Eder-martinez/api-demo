775777b581de7821f9a8ec0fd2b8ecf92db6775e
Eder-martinez <1722110109@utectulancingo.edu.mx>
2023-10-05 16:51:48 +0000

    dungeonsn

--- pruebas/ui/dungeons.py
+++ pruebas/ui/dungeons.py
@@ -1,13 +1,11 @@
 import requests
 import json
 
-URI = "https://www.dnd5eapi.co/api/classes"
+URI = "https://www.dnd5eapi.co/api/classes/?/proficiencies"
 response = requests.get(URI)
 #print(f"GET:{response.text}")
 response_json = json.loads(response.text)
 for i in range(12):
     print(f"{i+1}.-{response_json['results'][i]['name']}")
 
-
-    
 
